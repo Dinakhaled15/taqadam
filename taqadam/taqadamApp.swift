c9b905bc3be4f91c8554c4d9952fcf38f3527e62
“dinakhaled15” <dinakhaled1999@outlook.com>
2023-03-16 21:29:45 +0300

    Initial Commit

---
+++ taqadam/taqadamApp.swift
@@ -0,0 +1,17 @@
+//
+//  taqadamApp.swift
+//  taqadam
+//
+//  Created by Dina Alhajj Ibrahim on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct taqadamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
