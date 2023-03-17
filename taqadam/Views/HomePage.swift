a3a1de6f98e7f75ffaeccff4bd6a3d63896d9096
hotonMo1 <halharithi@waredat.sa>
2023-03-17 10:16:26 +0300

    -

--- taqadam/Views/HomePage.swift
+++ taqadam/Views/HomePage.swift
@@ -13,42 +13,46 @@ struct HomePage: View {
     @State var Country = ""
     
     var body: some View {
-        VStack(spacing : 20){
+        ScrollView{
             Image("MainpagePic")
-            Text("Hello, I’m here to help you get the career that you want")
-                .foregroundColor(.color)
-                .font(.system(size: 26))
-                .multilineTextAlignment(.center)
-                .padding(.horizontal,15)
-                .padding(.vertical,30)
-            
-            TextField("Enter The Job Title", text: $JobTitle)
-                .textFieldStyle(CaptionTextFieldStyle())
-                .padding(.horizontal)
-            
-            TextField("Enter the name of the Company", text: $CompanyName)
-                .textFieldStyle(CaptionTextFieldStyle())
-                .padding(.horizontal)
-            
-            TextField("Enter the Country desired", text: $Country)
-                .textFieldStyle(CaptionTextFieldStyle())
-                .padding(.horizontal)
-            
-         Spacer()
-            Button{
+                .resizable()
+                .frame(height: 230)
+            VStack(spacing : 20){
+           
+                Text("Hello, I’m here to help you get the career that you want")
+                    .foregroundColor(.color)
+                    .font(.system(size: 26))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal,15)
+                    .padding(.vertical,30)
+       
+                TextField("Enter The Job Title", text: $JobTitle)
+                    .textFieldStyle(CaptionTextFieldStyle())
+        
+                TextField("Enter the name of the Company", text: $CompanyName)
+                    .textFieldStyle(CaptionTextFieldStyle())
+               
                 
-            }label: {
-                ZStack{
-                    Rectangle()
-                        .foregroundColor(.color)
-                        .frame(height: 55)
-                        .cornerRadius(14)
-                    Text("Result")
-                        .foregroundColor(.white)
-                        .font(.system(size: 25))
-                }.padding(.horizontal)
-            }
-            .padding(.bottom)
+                TextField("Enter the Country desired", text: $Country)
+                    .textFieldStyle(CaptionTextFieldStyle())
+            
+                
+                Spacer()
+                Button{
+                    
+                }label: {
+                    ZStack{
+                        Rectangle()
+                            .foregroundColor(.color)
+                            .frame(height: 55)
+                            .cornerRadius(14)
+                        Text("Result")
+                            .foregroundColor(.white)
+                            .font(.system(size: 25))
+                    }
+                }
+        
+            }.padding(.horizontal)
         }
     }
 }
